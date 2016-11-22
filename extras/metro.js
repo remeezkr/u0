fb0b28b6ccf8eb1d3062dc22956772b2f8574e09
remeezkr <remeezkr@gmail.com>
2016-11-22 20:02:05 +0530

    Add files via upload

---
+++ extras/metro.js
@@ -0,0 +1,214 @@
+function closeTile_t1()
+{
+	
+	document.getElementById("t1").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t2()
+{
+	document.getElementById("t2").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t3()
+{
+	document.getElementById("t3").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t4()
+{
+	document.getElementById("t4").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t5()
+{
+	document.getElementById("t5").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t6()
+{
+	document.getElementById("t6").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t7()
+{
+	document.getElementById("t7").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t8()
+{
+	document.getElementById("t8").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+function closeTile_t9()
+{
+	document.getElementById("t9").style.animation="closeTile 0.4s ease-out 1 normal forwards";
+	getBack();
+}
+
+function clicked_t1()
+{
+	//document.getElementById("metro").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.5s ease-out 1 normal forwards";
+	//setTimeout(function(){window.location="inner.html"},200);
+	setTimeout(function(){document.getElementById("t1").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t2()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t2").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t3()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t3").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t4()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t4").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t5()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t5").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t6()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t6").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t7()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t7").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t8()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t8").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+function clicked_t9()
+{
+	document.getElementById("tile1").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile2").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile3").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile4").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile5").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile6").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile7").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile8").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile9").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	document.getElementById("loginTile").style.animation="myfirst 0.3s ease-out 1 normal forwards";
+	setTimeout(function(){document.getElementById("t9").style.animation="tileload 0.7s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},100);
+
+}
+setTimeout(function load()
+{
+	setTimeout(function(){document.getElementById("tile1").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";},50);
+	setTimeout(function(){document.getElementById("tile2").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},100);
+	setTimeout(function(){document.getElementById("tile3").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},200);
+	setTimeout(function(){document.getElementById("tile4").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},100);
+	setTimeout(function(){document.getElementById("tile5").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},200);
+	setTimeout(function(){document.getElementById("tile6").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},200);
+	setTimeout(function(){document.getElementById("tile7").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},400);
+	setTimeout(function(){document.getElementById("tile8").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},500);
+	setTimeout(function(){document.getElementById("tile9").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},500);
+	setTimeout(function(){document.getElementById("loginTile").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},600);
+},100);
+
+function getBack()
+{
+	//document.getElementById("metro").style.animation="loadback 0.3s ease-out 1 normal forwards";
+	document.getElementById("tile1").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards";
+	setTimeout(function(){document.getElementById("tile2").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},50);
+	setTimeout(function(){document.getElementById("tile3").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},100);
+	setTimeout(function(){document.getElementById("tile4").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},50);
+	setTimeout(function(){document.getElementById("tile5").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},100);
+	setTimeout(function(){document.getElementById("tile6").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},150);
+	setTimeout(function(){document.getElementById("tile7").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},200);
+	setTimeout(function(){document.getElementById("tile8").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},250);
+	setTimeout(function(){document.getElementById("tile9").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},250);
+	setTimeout(function(){document.getElementById("loginTile").style.animation="myload 1s cubic-bezier(.66,.04,.36,1.03) 1 normal forwards"},300);
+	
+}
